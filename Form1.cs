4d42f265d55a55ac9667586933ded0422e633823
Tomas Prochorenko <s034008@ad.viko.lt>
2019-11-12 13:50:33 +0200

    Sukurta prisijungimo klase

--- Form1.cs
+++ Form1.cs
@@ -11,13 +11,11 @@ using System.Windows.Forms;
 
 
 
+
 namespace RenginiuBilietai2
 {
     public partial class Form1 : Form
     {
-
-        MySqlConnection con = new MySqlConnection("server = localhost; user id = root; password=Lego3356; database=newdatabase");
-        int i;
 
         public Form1()
         {
@@ -40,28 +38,21 @@ namespace RenginiuBilietai2
 
         private void buttonPrisijungimas_Click(object sender, EventArgs e)
         {
-            i = 0;
-            con.Open();
+            bool prisijungimas;
 
-            MySqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from logininfo where user='" + textBoxPavadinimas.Text + "' and password='" + textBoxSlaptazodis.Text + "'";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-            da.Fill(dt);
-            i = Convert.ToInt32(dt.Rows.Count.ToString());
-            if (i == 0)
-            {
-                MessageBox.Show("Neteisingas slaptažodis arba prisijungimo vardas");
+            Adminas adm = new Adminas();
+            prisijungimas = adm.Login(textBoxPavadinimas.Text, textBoxSlaptazodis.Text);
 
-            }
-            else
+            if ( prisijungimas )
             {
                 MessageBox.Show("Sėkmingai prisijungta");
             }
+            else
+            {
+                MessageBox.Show("Blogai ivestas slaptazodis arba prisijungimo vardas");
+            }
 
-            con.Close();
+            
         }
     }
 }
---
+++ Prisijungimas.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenginiuBilietai2
+{
+    class Prisijungimas
+    { 
+        private int PrisijungimoID;
+        private string VartotojoVardas;
+        private string Slaptazodis;
+        private bool Ijungtas;
+
+        MySqlConnection con = new MySqlConnection("server = localhost; user id = root; password=Lego3356; database=newdatabase");
+        
+
+        public bool Login(string username, string password)
+        {
+           int i = 0;
+            con.Open();
+
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from logininfo where user='" + username + "' and password='" + password + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+            if (i == 0)
+            {
+                return false;
+                //
+
+            }
+            else
+            {
+                //MessageBox.Show("Sėkmingai prisijungta");
+                return true;
+            }
+
+            con.Close();
+
+
+            return false;
+        }
+        public bool Logout()
+        {
+            return true;
+        }
+
+    }
+
+
+}
